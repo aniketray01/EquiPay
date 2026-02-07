95054c842d9ef0e4da9d12955dc40c24c7f73e30
Aniket Ray <aniketray15@gmail.com>
2026-02-08 03:29:12 +0530

    debug: add console logs to check Firebase config in production

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,9 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
+console.log('Firebase Config:', firebaseConfig);
+console.log('API Key exists:', !!firebaseConfig.apiKey);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
